dd5ccf0dbc09e74610ec5af9019ce9a418bff2a1
alexey-ringo <alexey.ringo@gmail.com>
2019-01-09 12:30:30 +0000

    Added basic auth

--- public/index.php
+++ public/index.php
@@ -33,6 +33,8 @@ $routes->get('home', '/', Action\HelloAction::class);
 
 $routes->get('about', '/about', Action\AboutAction::class);
 
+$routes->get('cabinet', '/cabinet', Action\CabinetAction::class);
+
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 
 $routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
---
+++ src/App/Http/Action/CabinetAction.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Action;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+
+class CabinetAction {
+    public function __invoke(ServerRequestInterface $request) {
+        return new HtmlResponse('Cabinet.');
+    }
+}
