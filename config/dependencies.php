59595a7a2e1524b5710e5f6732cdc13cb0fce8c3
alexey-ringo <alexey.ringo@gmail.com>
2019-08-05 09:57:14 +0000

    show comment details

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,8 +3,8 @@
 use App\Http\Middleware;
 use Framework\Http\Application;
 use Framework\Http\Pipeline\MiddlewareResolver;
-use Framework\Http\Router\AuraRouterAdapter;
-//use Framework\Http\Router\SimpleRouter\SimpleRouter;
+//use Framework\Http\Router\AuraRouterAdapter;
+use Framework\Http\Router\SimpleRouter\SimpleRouter;
 use Framework\Http\Router\RouterInterface;
 use Zend\Diactoros\Response;
 use App\Http\Middleware\BasicAuthMiddleware;
@@ -28,16 +28,16 @@ return [
                     new NotFoundHandler()
                 );
             },
-            
+            /*
             RouterInterface::class => function() {
                 return new AuraRouterAdapter(new Aura\Router\RouterContainer());
             },
+            */
             
-            /*
             RouterInterface::class => function() {
                 return new SimpleRouter(new Framework\Http\Router\SimpleRouter\RouteCollection());
             },
-            */
+            
             
             MiddlewareResolver::class => function (ContainerInterface $container) {
                 return new MiddlewareResolver($container, new Response());
